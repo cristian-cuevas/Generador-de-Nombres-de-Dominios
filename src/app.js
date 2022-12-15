edb69ad491755645860f5a161bbeed267bd198b5
cristian-cuevas <cuevasgarciascristian@gmail.com>
2022-12-15 16:19:55 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+*/
+let prefijo = ["grande", "importante", "super", "muy", "mucho"];
+let palabra = ["codigo", "tecnologia", "pila", "computador", "pc"];
+console.log(palabra);
+// for (let a = 0; a < prefijo.length; a++) {
+// for (let b = 0; b < palabra.length; b++) {
+// let p = document.createElement("p");
+
+// div.innerText = `${prefijo[a]}${palabra[palabra].com} `;
+// document.body.appendChild(p);
+// }
+// }
+
+prefijo.map(e => {
+  palabra.map(b => {
+    let p = document.createElement("p");
+    p.innerText = `${e}${b}.com `;
+    document.body.appendChild(p);
+    console.log(p.innerText);
+  });
+});
